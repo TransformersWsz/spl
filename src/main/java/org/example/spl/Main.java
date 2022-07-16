2427c598d0de79b35771034b58c5513d932bdf86
TransformersWsz <3287124026@qq.com>
2022-07-17 01:35:15 +0800

    initially run project

---
+++ src/main/java/org/example/spl/Main.java
@@ -0,0 +1,14 @@
+package org.example.spl;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        Customer customerBean = (Customer) context.getBean("customerBean");
+        customerBean.displayInfo();
+
+        ((ClassPathXmlApplicationContext) context).close();
+    }
+}
